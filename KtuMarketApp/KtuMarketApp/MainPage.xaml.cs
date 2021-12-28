aef951a51c0990026484848e3e8c70a375ab6a1b
Berkan Çelik <berkan.celik.190515@gmail.com>
2021-12-28 17:22:14 +0300

    Uygulamanın Temelleri Atıldı.

--- KtuMarketApp/KtuMarketApp/MainPage.xaml.cs
+++ KtuMarketApp/KtuMarketApp/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KtuMarketApp.Views.PromotionPages;
+using KtuMarketApp.Views.SignUp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -14,5 +16,17 @@ namespace KtuMarketApp
         {
             InitializeComponent();
         }
+
+        private async void GoToPromotions(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Promotions());
+        }
+        
+        private async void GoToSignUp(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+
+
     }
 }
